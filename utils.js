7d96307531eae168b560dc0a2d77ffdbf84dd642
Lionello Lunesu <lio+git@lunesu.com>
2017-07-04 16:42:31 +0800

    Added sha256, hexToJwk, jwkToHex, jwkToPem, generateNonce, createEcdsaJws

--- utils.js
+++ utils.js
@@ -1,10 +1,11 @@
 //
 //  utils.js
 //
-//  Copyright © 2016 Trusted Key Solutions. All rights reserved.
+//  Copyright © 2017 Trusted Key Solutions. All rights reserved.
 //
 
 const Crypto    = require('crypto')
+const Assert    = require('assert')
 const Jsrsasign = require('jsrsasign')
 
 /**
@@ -13,6 +14,19 @@ const Jsrsasign = require('jsrsasign')
  * @exports utils
 */
 const utils = module.exports = {}
+
+
+/**
+ * Get the SHA256 of the specified blob
+ *
+ * @param {String|Buffer} blob - String or Buffer
+ * @param {String} encoding - OPTIONAL
+ * @returns {Buffer} Buffer with SHA256
+ */
+utils.sha256 = function(blob, encoding) {
+    var hash = Crypto.createHash('sha256')
+    return hash.update(blob).digest(encoding)
+}
 
 /**
  * Convert a certificate serial number to blockchain address
@@ -28,18 +42,19 @@ utils.serialToAddress = function(serialhex) {
 /**
  * Base64 encode URL string
  *
- * @param {String} urlString - URL string to encode
+ * @param {String} data - URL string to encode
+ * @param {String} encoding - (optional) encoding of data
  * @returns {String} base64-encoded URL
  */
-utils.base64url = function(urlString) {
-    return Buffer.from(urlString).toString("base64").replace(/=/g,'').replace(/\//g,'_').replace(/\+/g,'-')
+utils.base64url = function(data, encoding) {
+    return Buffer.from(data, encoding).toString("base64").replace(/=/g,'').replace(/\//g,'_').replace(/\+/g,'-')
 }
 
 /**
  * Get UTC seconds since UNIX epoch or convert date into unix time
  *
  * @param {Date} date - Optional date object
- * @returns {number} Unix timestamp
+ * @returns {Number} Unix timestamp
  */
 utils.getUnixTime = function(date) {
     return Math.floor((date||new Date).getTime()/1000)
@@ -49,9 +64,9 @@ utils.getUnixTime = function(date) {
 /**
  * Create a JSON Web Signature
  *
- * @param {object} message - Message can be string or object. Objects will be JSON stringified
+ * @param {object|String} message - Message can be string or object. Objects will be JSON stringified
  * @param {String} secret - HMAC shared secret
- * @param {object} [header={alg: "HS256"}] - JOSE header
+ * @param {object} [header={alg: "HS256"}] - JOSE header OPTIONAL
  * @returns {String} Concatenated JWS HMAC
  */
 utils.createHmacJws = function(message, secret, header) {
@@ -66,6 +81,30 @@ utils.createHmacJws = function(message, secret, header) {
     const jws = utils.base64url(jose) + '.' + utils.base64url(message)
     const hmac = Crypto.createHmac('sha256', secret)
     return jws + '.' + utils.base64url(hmac.update(jws, secret).digest())
+}
+
+
+/**
+ * Create a JSON Web Signature
+ *
+ * @param {object|String} message - Message can be string or object. Objects will be JSON stringified
+ * @param {object} credential - key pair
+ * @param {object} [header={alg: "ES256"}] - JOSE header OPTIONAL
+ * @returns {String} Concatenated JWS
+ */
+utils.createEcdsaJws = function(message, credential, header) {
+    if (typeof message !== 'string') {
+        message = JSON.stringify(message)
+    }
+    if (typeof header === 'undefined') {
+        header = {}
+    }
+    header.alg = "ES256"
+    const jose = JSON.stringify(header)
+    const jws = utils.base64url(jose) + '.' + utils.base64url(message)
+    // Sign a digest value
+    const sig = credential.signWithMessageHash(utils.sha256(jws, 'hex'))
+    return jws + '.' + utils.base64url(Buffer.from(sig,'hex'))
 }
 
 
@@ -89,41 +128,105 @@ utils.verifyJws = function(jws, secretCallback) {
     const signature = Buffer.from(parts[2], "base64")
     const signeddata = parts[0] + '.' + parts[1]
 
-    if (jose.alg === 'ES256') {
-        // ECDSA-SHA256
-        const claims = JSON.parse(message)
-        // Subject public key is stored in 'sub' claim
-        if (utils.checkECDSA("secp256r1", signeddata, secretCallback(jose, claims), signature.toString('hex'))) {
-            return claims
-        }
-        return false
-    }
-    else if (jose.alg === 'HS256') {
-        // HMAC-SHA256
-        const hmac = Crypto.createHmac('sha256', secretCallback(jose))
-        if (hmac.update(signeddata).digest().equals(signature)) {
-            // Verify any nested JWT
-            if (jose.cty === "JWT") {
-                return utils.verifyJws(message.toString(), secretCallback)
-            }
-            else {
-                return JSON.parse(message.toString())
+    function verify(secret) {
+        if (jose.alg === 'ES256') {
+            // ECDSA-SHA256
+            const payload = JSON.parse(message)
+            // OLD: Subject public key is stored in 'sub' claim
+            if (utils.checkECDSA("secp256r1", signeddata, secret||payload.sub, signature)) {
+                return payload
             }
         }
-    }
-    else if (jose.alg === 'RS256') {
-        // TODO
-        return false
-    }
-    else if (jose.alg === 'none') {
-        // NONE, only allow if callback returns empty string
-        const claims = JSON.parse(message)
-        if (signature === '' && secretCallback(jose, claims) === '') {
-            return claims
+        else if (jose.alg === 'HS256') {
+            // HMAC-SHA256
+            const hmac = Crypto.createHmac('sha256', secret)
+            if (hmac.update(signeddata).digest().equals(signature)) {
+                // Verify any nested JWT
+                if (jose.cty === "JWT") {
+                    return utils.verifyJws(message, secretCallback)
+                }
+                else {
+                    return JSON.parse(message)
+                }
+            }
         }
+        else if (jose.alg === 'RS256') {
+            // TODO
+        }
+        else if (jose.alg === 'none') {
+            // NONE, only allow if callback returns empty string
+            if (signature === '' && secret === '') {
+                return JSON.parse(message)
+            }
+        }
+        return false
     }
 
-    return false
+    const secret = typeof secretCallback === "function" ? secretCallback(jose) : secretCallback
+    if (typeof secret.then === "function") {
+        return secret.then(verify)
+    }
+    else {
+        return verify(secret)
+    }
+}
+
+
+/**
+ * Convert a JWK into a hex public key
+ * @param {String} jwk - JSON Web Key for public EC key
+ * @return {String} string with hex public key
+ */
+utils.jwkToHex = function(jwk) {
+
+    if (jwk.crv === "P-256" && jwk.kty === "EC") {
+        Assert.strictEqual(typeof jwk.d, "undefined")
+        // Convert x,y coordinates from JWK to hex encoded public key
+        const hex = "04" + Buffer.from(jwk.x, "base64").toString("hex") + Buffer.from(jwk.y, "base64").toString("hex")
+        Assert.strictEqual(hex.length, 130)
+        return hex
+    }
+    Assert(false, "Unsupported JWK:"+jwk)
+}
+
+
+/**
+ * Convert a hex public key into JWK
+ * @param {String} pubKeyHex - hex encoded public key
+ * @return {String} JSON Web Key
+ */
+utils.hexToJwk = function(pubKeyHex) {
+    Assert.strictEqual(pubKeyHex.length, 130)
+    Assert(pubKeyHex.startsWith("04"))
+
+    const buffer = Buffer.from(pubKeyHex, 'hex')
+    return {
+        "kty": "EC",
+        "crv": "P-256",
+        "x": utils.base64url(buffer.slice(1,33)),
+        "y": utils.base64url(buffer.slice(33)),
+    }
+}
+
+
+/**
+ * Convert a JWK into a hex public key
+ * @param {String} jwk - JSON Web Key for public EC key
+ * @return {String} string with PEM public key
+ */
+utils.jwkToPem = function(jwk) {
+
+    if (jwk.crv === "P-256" && jwk.kty === "EC") {
+        Assert.strictEqual(typeof jwk.d, "undefined")
+        // Convert x,y coordinates from JWK to base64 encoded public key
+        const all = Buffer.concat([Buffer.from(jwk.x, "base64"), Buffer.from(jwk.y, "base64")]).toString("base64")
+        Assert.strictEqual(all.length, 88)
+        // Basic template for the PEM; we'll overwrite the coordinates in-place
+        return `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE${all}
+-----END PUBLIC KEY-----`
+    }
+    Assert(false, "Unsupported JWK:"+jwk)
 }
 
 
@@ -132,17 +235,23 @@ utils.verifyJws = function(jws, secretCallback) {
  *
  * @param {String} curveName - Curve name (secp256r1)
  * @param {String} message - Message payload
- * @param {String} pubkey - Public key to check signature against
- * @param {String} signature - Signature payload
+ * @param {String|Object} pubkey - Public key to check signature against (hex)
+ * @param {Buffer|String} signature - Signature payload (hex)
  * @return {boolean} Indicate whether signature is correct
  */
 utils.checkECDSA = function(curveName, message, pubkey, signature) {
 
     // Verify a digest value
-    var hash = Crypto.createHash('sha256')
-    var digest = hash.update(message).digest().toString('hex')
+    var digest = utils.sha256(message, 'hex')
 
-    var curve = new Jsrsasign.KJUR.crypto.ECDSA({xy:pubkey,curve:curveName})
+    if (pubkey.kty) {
+        pubkey = utils.jwkToHex(pubkey)
+    }
+    if (signature instanceof Buffer) {
+        signature = signature.toString('hex')
+    }
+
+    var curve = new Jsrsasign.KJUR.crypto.ECDSA({xy: pubkey, curve: curveName})
     return curve.verifyHex(digest, signature, pubkey)
 }
 
@@ -155,11 +264,10 @@ utils.checkECDSA = function(curveName, message, pubkey, signature) {
  */
 utils.userPubKeyHexToAddress = function(pubkeyhex) {
     // Sign a digest value
-    var hash = Crypto.createHash('sha256')
     // Get the uncompressed public key without prefix, take the sha256 hash, and skip the first 12 bytes
     var blob = new Buffer(pubkeyhex.substr(2), 'hex')
-    var digest = hash.update(blob).digest()
-    return "0x" + digest.toString('hex').substr(2*12)
+    var digest = utils.sha256(blob, 'hex')
+    return "0x" + digest.substr(2*12)
 }
 
 
@@ -173,8 +281,18 @@ utils.promisify = function(call) {
         var args = Array.prototype.slice.call(arguments);
         return new Promise( (resolve,reject) => {
             // Append the callback that either rejects or resolves the promise
-            args.push( (err,result) => err?reject(err):resolve(result) );
+            args.push( (err,a,b,c,d) => err?reject(err):resolve(a,b,c,d) );
             call.apply(self, args);
         })
     }
+}
+
+
+/**
+ * Generate a 32-byte random nonce.
+ * @param {String} encoding - Encoding for result (default base64)
+ * @returns {String} The encoding of the nonce
+ */
+utils.generateNonce = function(encoding) {
+    return Crypto.randomBytes(32).toString(encoding || 'base64')
 }
