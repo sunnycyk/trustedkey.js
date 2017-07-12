c1a74592e42b30b04723961e5a109520086f5a60
Lionello Lunesu <lio+git@lunesu.com>
2017-07-12 10:33:19 +0800

    Fix: merge query from path with new params

--- services/http.js
+++ services/http.js
@@ -1,27 +1,36 @@
+const RP          = require('request-promise-native')
+const URL         = require('url')
+
+const Utils       = require('../utils')
+
 const httpUtils = module.exports = function(backendUrl, appId, appSecret) {
     this.backendUrl = backendUrl
     this.appId = appId
     this.appSecret = appSecret
 }
 
-const Querystring = require('querystring')
-const RP          = require('request-promise-native')
-
-const Utils       = require('../utils')
-
-
 function getAuthHeader(url, appId, appSecret) {
     const payload = {
         iss: appId,
         aud: url,
+        exp: Utils.getUnixTime()+300
     }
-    const header = {typ: 'JWT', iss: appId, exp: Utils.getUnixTime()+300}
+    const header = {typ: 'JWT', iss: appId }
     return 'Bearer ' + Utils.createHmacJws(payload, appSecret, header)
 }
 
 
+function mergeParams(path, params) {
+    const url = URL.parse(path, true)
+    Object.assign(url.query, params)
+    delete url.search   // force recreation from .query
+    return  url.format()
+}
+
+
 httpUtils.prototype.get = function(path, params) {
-    const url = path + '?' + Querystring.stringify(params)
+
+    const url = mergeParams(path, params)
     const headers = {}
     if(this.appId && this.appSecret) {
         headers['Authorization'] = getAuthHeader(this.backendUrl + url, this.appId, this.appSecret)
@@ -35,8 +44,10 @@ httpUtils.prototype.get = function(path, params) {
     })
 }
 
+
 httpUtils.prototype.post = function(path, params) {
-    const url = path + '?' + Querystring.stringify(params)
+
+    const url = mergeParams(path, params)
     const headers = {}
     if(this.appId && this.appSecret) {
         headers['Authorization'] = getAuthHeader(this.backendUrl + url, this.appId, this.appSecret)
