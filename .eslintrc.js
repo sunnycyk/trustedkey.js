e3db31ac4c0d3a7c5fa74ef61eafe671ef8a54f0
Lionello Lunesu <lio+git@lunesu.com>
2017-08-03 13:56:59 +0800

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "plugins": [
+        "hapi"
+    ],
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-multiple-empty-lines": [
+          "error",
+          { "max": 2, "maxBOF": 0, "maxEOF": 1 }
+        ],
+        "hapi/hapi-capitalize-modules": [
+          "error"
+        ],
+        "no-unused-vars": [
+            "error",
+            { "vars": "all", "args": "none"}
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "off",
+            "double"
+        ],
+        "semi": [
+            "warn",
+            "never"
+        ],
+        "no-console": ["warn"],
+    }
+};
